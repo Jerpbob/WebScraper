c5f1c5463d30dc0149708042d54e12c36a61b06f
Jerpbob <jerupaulbalares@gmail.com>
2023-05-23 23:04:46 -0700

    Found the author text in the parsed html and isolated it from its surrounding text

--- webscrape.py
+++ webscrape.py
@@ -6,8 +6,9 @@ baseUrl = "https://mangakakalot.com/search/story/"
 
 #FUTURETODO: Find a way to implement a frontend that takes input from the user
 #and then sends that input into this script (or a future backend)
-userSearch = input()
+# userSearch = input()
 
+userSearch = 'one punch man'
 r = requests.get(baseUrl + userSearch.replace(" ", "_"))
 soup = BeautifulSoup(r.text, 'html.parser')
 print("Grabbing from " + baseUrl + userSearch.replace(" ", "_"))
@@ -18,9 +19,15 @@ body = soup.body
 links_in_body = body.find_all(class_="story_item")
 
 for links in links_in_body:
-    # print(links.a.prettify())
+    #print(links.a.prettify())
     # print(links.a.img.prettify())
-    print(links.a.img['alt'])
+    link = links.a['href']
+    linkName = links.a.img['alt']
+    author = links.div.span.text
+    print(link)
+    print(linkName)
+    print(author[author.index(':') + len(': '):])
+    print()
 
 #TODO: Put the title, link, and other info (author, num of chapters) into variables
 #TODO: And then put them into a dictionary, to then transform it into JSON to send to the frontend
