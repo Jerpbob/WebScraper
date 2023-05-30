7299131bc86535885a574a76030944ac0c90db14
Jerpbob <jerupaulbalares@gmail.com>
2023-05-30 13:38:03 -0700

    Added code to extract tag information from the mangakakalot url. Also added some 'try-except' code to ignore the uncommon urls that mangakakalot puts for some reason such as "mangakakalot/read-"

--- src/flask/webscraper/webscrape.py
+++ src/flask/webscraper/webscrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import lxml
 
 
 # TODO: implement a way so that the script scrapes from several websites
@@ -17,53 +18,71 @@ def mangakakalot_manga_webscraper(user_search = "kaguya sama"):
     # simply using the request library to send a request to the url in order to start extracting data
     # with beautifulsoup
     r = requests.get(base_url + user_search.replace(" ", "_"))
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'lxml')
     print("Grabbing from " + base_url + user_search.replace(" ", "_"))
     body = soup.body
     links_in_body = body.find_all(class_="story_item")
 
     for links in links_in_body:
-        # print(links.a.prettify())
-        # print(links.a.img.prettify())
+        try:
+            # print(links.a.prettify())
+            # print(links.a.img.prettify())
 
-        # create a dictionary to put the extracted data into
-        # and also create a list for the tags to then put into the list under one value: 'tags'
-        link_dict = dict()
-        tag_list = list()
+            # create a dictionary to put the extracted data into
+            # and also create a list for the tags to then put into the list under one value: 'tags'
+            link_dict = dict()
+            tag_list = list()
 
-        # simply navigating through the html
-        link = links.a['href']
-        link_name = links.a.img['alt']
-        author = links.div.span.text
-        author = author[author.index(':') + len(': '):]
-        manga_id = ''
-        manga_site = ''
+            # simply navigating through the html
+            link = links.a['href']
+            link_name = links.a.img['alt']
+            author = links.div.span.text
+            author = author[author.index(':') + len(': '):]
+            manga_id = ''
+            manga_site = ''
 
-        # Since mangakakalot makes their manga search results have links that apply to either the mangakakalot site
-        # or the manganato site, separate processes were created to extract certain elements(such as tags) as
-        # the html templates were constructed differently
-        if 'manganato' in link:
-            manga_id = link[link.index('manga-') + len('manga-'):]
-            manga_site = 'manganato'
-            link_url = requests.get(link)
-            print(f"Grabbing from {link}")
-            soup2 = BeautifulSoup(link_url.text, "html.parser")
-            tags = soup2.body.find(class_='body-site').find(class_='container-main').find(class_='container-main-left') \
-                .find(class_='panel-story-info').tbody.contents[7].contents[3].find_all('a')
-            for tag in tags:
-                tag_list.append(tag.text)
-        elif 'mangakakalot' in link:
-            manga_id = link[link.index('manga/') + len('manga/'):]
-            manga_site = 'mangakakalot'
-        # manga_id = links[links.index('')]
-        # putting extracted html elements into dictionary keys
-        link_dict["site"] = manga_site
-        link_dict["id"] = manga_id
-        link_dict["name"] = link_name
-        link_dict["author"] = author
-        link_dict["link"] = link
-        link_dict["tags"] = tag_list
-        link_list.append(link_dict)
+            # Since mangakakalot makes their manga search results have links that apply to either the mangakakalot site
+            # or the manganato site, separate processes were created to extract certain elements(such as tags) as
+            # the html templates were constructed differently
+            if 'manganato' in link:
+                try:
+                    link_url = requests.get(link)
+                    soup2 = BeautifulSoup(link_url.text, "lxml")
+                    manga_id = link[link.index('manga-') + len('manga-'):]
+                    manga_site = 'manganato'
+                    tags = soup2.body.find(class_='body-site').find(class_='container-main') \
+                        .find(class_='container-main-left').find(class_='panel-story-info') \
+                        .tbody.contents[7].contents[3].find_all('a')
+                    for tag in tags:
+                        tag_list.append(tag.text)
+                except IndexError:
+                    continue
+                except ValueError:
+                    continue
+            elif 'mangakakalot' in link:
+                try:
+                    link_url = requests.get(link)
+                    soup2 = BeautifulSoup(link_url.text, "lxml")
+                    manga_id = link[link.index('manga/') + len('manga/'):]
+                    manga_site = 'mangakakalot'
+                    tags = soup2.body.contents[3].find(class_='leftCol').ul.contents[13].find_all('a')
+                    for tag in tags:
+                        tag_list.append(tag.text)
+                except IndexError:
+                    continue
+                except ValueError:
+                    continue
+            # manga_id = links[links.index('')]
+            # putting extracted html elements into dictionary keys
+            link_dict["site"] = manga_site
+            link_dict["id"] = manga_id
+            link_dict["name"] = link_name
+            link_dict["author"] = author
+            link_dict["link"] = link
+            link_dict["tags"] = tag_list
+            link_list.append(link_dict)
+        except ValueError:
+            continue
 
     for di in link_list:
         json_object = json.dumps(di, indent=4)
