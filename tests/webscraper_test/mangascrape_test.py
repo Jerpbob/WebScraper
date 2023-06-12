ac8b98f7b886cdedf4c592b3c7d4ea0681fa81fc
Jerpbob <jerupaulbalares@gmail.com>
2023-06-12 15:49:00 -0700

    Changed the Mangascrape class into a simple function called grab_chap_info and added tests to it

--- tests/webscraper_test/mangascrape_test.py
+++ tests/webscraper_test/mangascrape_test.py
@@ -5,37 +5,55 @@ import lxml
 import json
 
 
-class TestMangascraper:
+def grab_chap_info(manga_id):
     manga_info_dict = dict()
-    name = ''
     author_s = list()
-    status = ''
     tags = list()
 
-    def __init__(self, manga_id):
-        base_url = "https://chapmanganato.com/manga-"
-        link_url = base_url + manga_id
-        link = requests.get(link_url)
-        soup = BeautifulSoup(link.text, "lxml")
-        soup = soup.body.find(class_="body-site")\
-            .find(class_="container-main").find(class_="panel-story-info").find(class_="story-info-right")
-        manga_info = soup.tbody.find_all(class_="table-value")
-        self.name = soup.h1.text
-        authors = manga_info[1].a.contents
-        for a in authors:
-            self.author_s.append(a)
-        self.status = manga_info[2].text
-        tag = manga_info[3].find_all("a")
-        for t in tag:
-            self.tags.append(t.text)
+    base_url = "https://chapmanganato.com/manga-"
+    link_url = base_url + manga_id
+    link = requests.get(link_url)
+    soup = BeautifulSoup(link.text, "lxml")
+    soup = soup.body.find(class_="body-site") \
+        .find(class_="container-main").find(class_="panel-story-info").find(class_="story-info-right")
+    manga_info = soup.tbody.find_all(class_="table-value")
+    name = soup.h1.text
+    authors = manga_info[1].find_all("a")
+    for a in authors:
+        author_s.append(a.text)
+    status = manga_info[2].text
+    tag = manga_info[3].find_all("a")
+    for t in tag:
+        tags.append(t.text)
 
-        self.manga_info_dict["Name"] = self.name
-        self.manga_info_dict["Author/s"] = self.author_s
-        self.manga_info_dict["Status"] = self.status
-        self.manga_info_dict["Tag/s"] = self.tags
-        manga_json = json.dumps(self.manga_info_dict)
-        print(manga_json)
+    manga_info_dict["Name"] = name
+    manga_info_dict["Author/s"] = author_s
+    manga_info_dict["Status"] = status
+    manga_info_dict["Tag/s"] = tags
+    manga_json = json.dumps(manga_info_dict)
+    return manga_json
 
 
-test = TestMangascraper("nc952011")
+class TestMangascraper:
 
+    def test1(self):
+        assert grab_chap_info("nc952011") == \
+               '{"Name": "Kaguya-Sama Wa Kokurasetai - Tensai-Tachi No Renai Zunousen",' \
+               ' "Author/s": ["Akasaka Aka"],' \
+               ' "Status": "Ongoing",' \
+               ' "Tag/s": ["Comedy", "Drama", "Romance", "School life", "Seinen"]}'
+
+    def test2(self):
+        assert grab_chap_info("wd951838") == \
+               '{"Name": "Onepunch-Man",' \
+               ' "Author/s": ["One", "Murata Yuusuke"],' \
+               ' "Status": "Ongoing",' \
+               ' "Tag/s": ["Action", "Comedy", "Fantasy", "Mature", "Mystery", "Psychological", "Sci fi", "Seinen",' \
+               ' "Supernatural"]}'
+
+    def test3(self):
+        assert grab_chap_info("nu990877") == \
+               '{"Name": "Kaoru Hana Wa Rin To Saku",' \
+               ' "Author/s": ["Mikami Saka"],' \
+               ' "Status": "Ongoing",' \
+               ' "Tag/s": ["Comedy", "Drama", "Romance", "School life", "Shounen"]}'
