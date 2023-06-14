ddc7ccadca8cc07286c172388e5a8d3634cdb17f
Jerpbob <jerupaulbalares@gmail.com>
2023-06-13 18:30:41 -0700

    Modified current search_result function in app.py to take user input

--- src/flask/app.py
+++ src/flask/app.py
@@ -4,11 +4,17 @@ from webscraper.webscrape import search_scraper as ss
 app = Flask(__name__)
 
 
-@app.route("/user/search", methods=["GET"])
-def search_result():
+# @app.route("/user/search", methods=["GET"])
+# def search_result():
+#     if request.method == "GET":
+#         user_search = ss("Kaguya sama")
+#         return jsonify(user_search)
+
+@app.route("/user/search/<user_search>", methods=["GET"])
+def search_result(user_search):
     if request.method == "GET":
-        user_search = ss("Kaguya sama")
-        return jsonify(user_search)
+        user_search_result = ss(user_search)
+        return jsonify(user_search_result)
 
 
 if __name__ == '__main__':
